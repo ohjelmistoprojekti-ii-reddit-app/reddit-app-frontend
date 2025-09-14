39b4988b32967d7ef834eea9885618870839eb8a
niklasovaska <niklas.ovaska@gmail.com>
2025-09-14 19:10:11 +0300

    updated filtering

--- src/components/features/topic/FilterableGrid.tsx
+++ src/components/features/topic/FilterableGrid.tsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 import TopicFilter from "./TopicFilter";
 import TopicsGrid from "./TopicsGrid";
 import { TopicsGridProps, SentimentFilterType } from "@/types/topic.types";
+import { getSentimentLabel } from "@/lib/helpers/getSentimentLabel";
 
 export default function FilterableGrid({ topics } : TopicsGridProps) {
   const [filter, setFilter] = useState<SentimentFilterType>("all");
 
-  const filteredTopics = topics?.filter(t => (filter === "all" ? true : t.sentiment_scores.sentiment === filter)) ?? null
+  const filteredTopics = topics?.filter(t => (filter === "all" ? true : getSentimentLabel(t.sentiment_values.average_compound) === filter)) ?? null
+  
 
   return (
     <>
