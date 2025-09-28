b87005079a7655ec2de2c361410bf67482cacf44
niklasovaska <niklas.ovaska@gmail.com>
2025-09-29 00:27:13 +0300

    chart component attached to topicCard closes #11

---
+++ src/components/features/topic/SentimentChart.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { SentimentChartProps } from "@/types/topic.types"
+import { Pie, PieChart } from "recharts"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+export const description = "A donut chart"
+
+const chartConfig = {
+  positive: {
+    label: "Positive",
+    color: "var(--chart-2)",
+  },
+  neutral: {
+    label: "Neutral",
+    color: "var(--chart-3)",
+  },
+  negative: {
+    label: "Negative",
+    color: "var(--chart-5)",
+  },
+} satisfies ChartConfig
+
+export default function SentimentChart({ data }: SentimentChartProps) {
+    return (
+        <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+            >
+            <PieChart>
+                <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    innerRadius={60}
+                />
+                <ChartLegend 
+                    className="-translate-y-2 flex-wrap gap-2 *:basis-1/4 *:justify-center" 
+                    content={<ChartLegendContent nameKey="name" />}
+                />
+            </PieChart>
+        </ChartContainer>
+    )
+}
--- src/components/features/topic/TopicCard.tsx
+++ src/components/features/topic/TopicCard.tsx
@@ -1,20 +1,18 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { TopicCardProps } from "@/types/topic.types";
 import { DynamicIcon } from 'lucide-react/dynamic';
+import SentimentChart from "./SentimentChart";
 
 export default function TopicCard({ topic } : TopicCardProps) {
-    // Velocity, up/down/flat sign, pct and badge class commented out for until decided what to eventually display in TopicCard
+    
+    const chartData = [
+        { name: "positive", value: topic.sentiment_values.average_pos, fill: "var(--color-positive)" },
+        { name: "neutral", value: topic.sentiment_values.average_neu, fill: "var(--color-neutral)" },
+        { name: "negative", value: topic.sentiment_values.average_neg, fill: "var(--color-negative)" },
+    ];
 
-    // const v = topic.velocity ?? 0
     const c = topic.sentiment_values.average_compound
-    // const sign = v > 0 ? "↑" : v < 0 ? "↓" : "→"
-    // const pct = Math.round(Math.abs(v) * 100)
-    // const badgeClass =
-    // v > 0
-    //     ? "text-green-700 border-green-200"
-    //     : v < 0
-    //     ? "text-red-700 border-red-200"
-    //     : "text-gray-600 border-gray-200"
+    
     const smileyIcon =
     c >= 0.05
         ? "smile"
@@ -48,19 +46,15 @@ export default function TopicCard({ topic } : TopicCardProps) {
                 
             </CardHeader>
             <CardContent className="text-sm">
-                Positive: {topic.sentiment_values.average_pos} • 
+                {/* Positive: {topic.sentiment_values.average_pos} • 
                 Neutral: {topic.sentiment_values.average_neu} • 
-                Negative: {topic.sentiment_values.average_neg}
+                Negative: {topic.sentiment_values.average_neg} */}
+                <SentimentChart data={chartData}/>
             </CardContent>
             <CardFooter className="flex items-center gap-4 justify-between space-y-0 pb-2">
                 <div className="text-xs text-gray-500">
                     {topic.num_posts} posts • {topic.sentiment_values.comment_count} comments
                 </div>
-                {/* <span
-                    className={`text-xs rounded px-2 py-0.5 border ${badgeClass}`}
-                    >
-                    {pct} % {sign}
-                </span> */}
             </CardFooter>
         </Card>
         
