a801a935269500b7a22241c5cc9f0d683b52fdbd
niklasovaska <niklas.ovaska@gmail.com>
2025-09-30 12:53:01 +0300

    world map page first version with hovering countries

---
+++ src/app/map/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import WorldSvg from "../../../assets/world.svg"
+
+export default function WorldMap() {
+
+  return (
+    <div className="flex items-center justify-center w-full h-auto p-10">
+        <WorldSvg 
+            className="w-full h-auto"
+        />
+    </div>
+  );
+}
