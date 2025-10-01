48263fd56b369b23573b77a70c9aca17045c6583
niklasovaska <niklas.ovaska@gmail.com>
2025-10-01 18:36:07 +0300

    updated map types

--- src/types/map.types.ts
+++ src/types/map.types.ts
@@ -1,10 +1,23 @@
-export type CountryDetails = {
-    id: string
-    name: string | null
+export type MapDialogProps = {
+    subredditName: string
+    posts: CountryTopPost[]
 }
 
-export type MapDialogProps = {
-    details?: CountryDetails
-    open: boolean
-    setOpen: (open: boolean) => void
+export type MapDialogContentProps= {
+    posts: CountryTopPost[]
+}
+
+type CommentSentimentValues = {
+    sentiment_compound: number,
+    sentiment_neg: number,
+    sentiment_neu: number,
+    sentiment_pos: number,
+}
+
+export type CountryTopPost = {
+    post_title: string
+    comment_original: string
+    comment_eng: string
+    post_score: 1470
+    sentiment_values: CommentSentimentValues
 }
