8b05145df8df28cc41dcb408631e0b1b72602173
niklasovaska <niklas.ovaska@gmail.com>
2025-11-18 15:57:20 +0200

    remove console logs

--- src/lib/api/getLatestTopicsDb.ts
+++ src/lib/api/getLatestTopicsDb.ts
@@ -3,7 +3,6 @@ import { RedditTopic } from "@/types/topic.types"
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:5000";
 
 export async function getLatestTopicsDb(subredditName: string): Promise<RedditTopic[]> {
-    console.log('fetching...')
     
     const res = await fetch(`${BASE_URL}/api/topics/latest/${subredditName}`, {
         cache: "no-store"
@@ -12,9 +11,6 @@ export async function getLatestTopicsDb(subredditName: string): Promise<RedditTo
     if (!res.ok) throw new Error(`Request failed: ${res.status}`);
 
     const data = await res.json()
-
-    console.log('fetched')
-    console.log(data)
 
     return data
 }
