fc7fe8299adee0fef8755d80cd0ffc102cf55026
bhm530@myy.haaga-helia.fi <bhm530@myy.haaga-helia.fi>
2025-11-20 15:30:29 +0200

    refactor: enhance PostsDashboard layout.

--- src/components/subscription/PostsDashboard.tsx
+++ src/components/subscription/PostsDashboard.tsx
@@ -3,10 +3,10 @@ import { PostsSubscriptionData } from "@/types/subscription.types"
 
 export default function PostsDashboard({ data }: { data: PostsSubscriptionData }) {
     return(
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 p-10 w-full">
             {data.posts.map((post) => (
                 <SubscriptionPost key={post.id} post={post} />
             ))}
-        </>
+        </div>
     )
 }
